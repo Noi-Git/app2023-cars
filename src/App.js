5f8edc7ac35b0021a47e1f89a85e970003ba6e58
Noi-Git <noi.patsin@gmail.com>
2023-04-05 18:56:55 -0400

    display each component in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.css'
+import CarForm from './components/CarForm'
+import CarList from './components/CarList'
+import CarSearch from './components/CarSearch'
+import CarValue from './components/CarValue'
 
 function App() {
-  return <div className='App'>Hello</div>
+  return (
+    <div className='App'>
+      <CarForm />
+      <CarList />
+      <CarSearch />
+      <CarValue />
+    </div>
+  )
 }
 
 export default App

13b7fdcca948539caff0b685be4b4bdffa76763c
Noi-Git <noi.patsin@gmail.com>
2023-04-06 18:49:21 -0400

    pass payload to redux store when typing

--- src/components/CarSearch.jsx
+++ src/components/CarSearch.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { changeSearchTerm } from '../store'
 
 const CarSearch = () => {
-  return <div>CarSearch</div>
+  const dispatch = useDispatch()
+
+  const searchTerm = useSelector((state) => {
+    // state.searchTerm
+  })
+
+  const handleSearchTermChange = (event) => {
+    dispatch(changeSearchTerm(event.target.value)) //event.target.value = payload
+  }
+
+  return (
+    <div className='list-header'>
+      <h3 className='title is-3'> My Cars </h3>
+      <div className='search field is-horizontal'>
+        <label className='label'>Search</label>
+        <input
+          type='text'
+          className='input'
+          value={searchTerm}
+          onChange={handleSearchTermChange}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default CarSearch

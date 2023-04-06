9a5b850dd9d39f640f2284ec1d04090498103f22
Noi-Git <noi.patsin@gmail.com>
2023-04-05 20:21:35 -0400

    import all reducers and change folder structure

---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit'
+import {
+  carsReducer,
+  addCar,
+  removeCar,
+  changeSearchTerm,
+} from './slices/carsSlice'
+import { formReducer, changeName, changeCost } from './slices/formSlice'

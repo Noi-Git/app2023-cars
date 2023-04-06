15a0444ce35a1b67624f2a0c55b35af566894650
Noi-Git <noi.patsin@gmail.com>
2023-04-05 20:43:56 -0400

    Form 1 setup form

--- src/components/CarForm.jsx
+++ src/components/CarForm.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
 
 const CarForm = () => {
-  return <div>CarForm</div>
+  const handleNameChange = () => {}
+
+  return (
+    <div className='car-form panel'>
+      <h4 className='subtitle is-3'>Add Car</h4>
+      <form>
+        <div className='field-group'>
+          <div className='field'>
+            <label className='label'>Name</label>
+            <input
+              type='text'
+              className='input is-expanded'
+              // === is suppose to be name state coming out of redux store
+              // we need to access store - reach out to current name piece of state
+              // value={name}
+              onChange={handleNameChange}
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default CarForm

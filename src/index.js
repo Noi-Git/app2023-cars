35e131014f53e7d709552c30a883e36ee1250924
Noi-Git <noi.patsin@gmail.com>
2023-04-05 20:35:52 -0400

    wrap the <App> with provider and pass in store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { Provider } from 'react-redux/'
+import { store } from './store'
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
+  // wrap the <App/> with Provider
+  // and pass in the store props
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 )

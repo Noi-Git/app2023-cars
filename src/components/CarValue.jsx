78bfb0c2d98d6b53e4a954f643e91fdb527fc015
Noi-Git <noi.patsin@gmail.com>
2023-04-06 20:12:14 -0400

    refactor calculate total

--- src/components/CarValue.jsx
+++ src/components/CarValue.jsx
@@ -5,11 +5,16 @@ const CarValue = () => {
     const filteredCars = data.filter((car) => {
       return car.name.toLowerCase().includes(searchTerm.toLowerCase())
     })
-    let cost = 0
-    for (let car of filteredCars) {
-      cost += car.cost
-    }
-    return cost
+    // let cost = 0
+    // for (let car of filteredCars) {
+    //   cost += car.cost
+    // }
+    // return cost
+
+    // refactor to use accumulator function
+    return filteredCars.reduce((acc, car) => {
+      return acc + car.cost
+    }, 0)
   })
 
   return <div className='car-value'>Total Cost: ${totalCost}</div>

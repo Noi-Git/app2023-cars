dffe797340a9d7c9da001b0a843fc964f60dfb4e
Noi-Git <noi.patsin@gmail.com>
2023-04-06 20:28:22 -0400

    use the logic

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -23,8 +23,12 @@ const CarList = () => {
   const renderedCars = cars.map((car) => {
     // the matching cars will be bold when we rendered the car
     // we need to know the car's name -- and the current name user is typing -- state.form.name
+    // use for designing which care should be bold
+    // if name in the search is present -- do the following
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase())
     return (
-      <div key={car.id} className='panel'>
+      // if bold -- apply css bold
+      <div key={car.id} className={`panel ${bold && 'bold'}`}>
         <p>
           {car.name} - ${car.cost}
         </p>
